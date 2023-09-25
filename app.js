fbd219b2c773f69a7b3bee21decec51aa2a6b6db
gosha <78@.ru>
2023-09-25 21:54:50 +0500

    js-3

---
+++ app.js
@@ -0,0 +1,32 @@
+// Первая задача
+
+// let a = +prompt('Введите число')
+// let b = +prompt('Введите степень')
+// let c = 1
+
+// for (let i = 0; i < b; i++) {
+   
+//     c = c*a
+        
+// }
+
+
+// console.log(c);
+
+//Вторая задача
+
+// let a = +prompt('Введите коливество')
+// let c = prompt('Введите символ отступов')
+// let b = prompt('Введите конечный символ')
+// let res = ''
+// for (let i = 0; i < a; i++) {
+
+// if (i == 0) {
+//     res = c
+// }else{
+
+// res = b+res
+
+// }
+// console.log(res);
+// }
